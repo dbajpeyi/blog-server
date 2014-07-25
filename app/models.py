fb35393084bd10603b0861a46088b9a18815b280
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2014-07-25 12:12:05 +0000

    schema for post, author and comments

--- app/models.py
+++ app/models.py
@@ -1,19 +1,41 @@
 """
-todo.models
+app.models
 
 Schema for mongo documents we will store
 """
 from mongoengine import *
+import datetime
     
-class Todo(Document):
-    """Something we want to do"""
+class User(Document):
+    name        = StringField(max_length=50, required=True)
+    email       = EmailField(max_length=50, required=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Post(Document):
+    """A combination of title , content and author"""
     
-    activity = StringField(max_length=128, required=True)
-    completed = BooleanField(default=False)
+    author      = ReferenceField(User, required=True)
+    content 	= StringField(max_length=10000, required=True)
+    title       = StringField(max_length=250, required=True)
+    created     = DateTimeField(default=datetime.datetime.now(), required=True)
+    completed 	= BooleanField(default=False)
 
     meta = {
-        'indexes': ['activity']
+        'indexes': ['title', '-created']
     }
 
     def __str__(self):
-        return self.activity
+        return self.title
+
+
+
+class Comment(Document):
+    text        = StringField(max_length=255, required=True) 
+    user        = ReferenceField(User, required=True)
+
+    def __unicode__(self):
+        return "%s : %s"(user, text)
+
